07b6b5e9f0538c169dbd1e9677dd5aa65e84679f
Esteban <estebanddlp@gmail.com>
2017-11-22 20:40:06 -0300

    Add some files generated by VS.

--- WebApi/wwwroot/js/index.js
+++ WebApi/wwwroot/js/index.js
@@ -1,1 +1,16 @@
 ﻿console.log("test");
+
+$("#ingresar_palabra").submit(function (e) {
+
+    var url = "http://localhost:49207/api/values"; // the script where you handle the form input.
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (data) {
+            alert(data); // show response from the php script.
+        }
+    });
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+});
